b9c9eb1241946286b6c709348f05e5c50a56bf7e
SvdB-ICT <97052791@st.deltion.nl>
2026-02-05 16:38:12 +0100

    Gewerkt aan 020 Tekstfuncties

--- app/020_tekstfuncties/opdr_2/opdr_2.py
+++ app/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,11 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Stefan van den Berg
 # Groep:
 
 # Hier komt je code...
 
-my_list = ...
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"  
+count = my_string.lower().count("t")
 
-print()
+print("De letter \"t\" komt ", count, "x voor in my_string")
+exit()
--- app/020_tekstfuncties/opdr_3/opdr_3.py
+++ app/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,33 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
+# Naam student: Stefan van den Berg
 # Groep:
 
 # Hier komt je code...
+lines = {
+    "line1": ["    *", 0, ""], 
+    "line2": ["   ***", 0, ""],
+    "line3": ["  ******", 0, ""],
+    "line4": [" ********", 0, ""],
+    "line5": ["***********", 0, ""],
+    "line6": ["    ***", 0, ""],
+    "line7": ["    ***", 0, ""],
+    "line8": ["    ***", 0, ""]
+    }
+
+treeCount = 5
+blankSpaceCharacter = " "
+
+def DrawBaseTree():
+    for line in lines:
+        print(lines[line][0])
+
+def BalanceSpace():
+    print()
+
+def AddTree():
+    print()
+
+# Commands
+
+DrawBaseTree()
+quit()
